515975d931ea1ac73296d06be6f1b97e288a8830
Pheakboy <128297342+Pheakboy@users.noreply.github.com>
2026-05-06 15:36:53 +0700

    add yamal to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-   server: {
+  // ⚠️ Change '/OCR_Text_Extract/' to match your GitHub repository name exactly
+  base: '/OCR_Text_Extract/',
+  server: {
     host: true
   }
 });
